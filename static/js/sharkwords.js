501df9c5e7619c8fa1b756753a6820182c7d5d66
Melanie Tai <mt337@cornell.edu>
2022-04-05 12:36:24 -0700

    Completed createDivForChars

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,7 +20,14 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const char of word) {
+    const container = document.querySelector("#word-container")
+    container
+      .insertAdjacentHTML(
+        'beforeend',
+        `<div class="letter-box ${char}"></div>`
+      );
+  };
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
@@ -56,7 +63,7 @@ const isLetterInWord = (letter) => {
   const word = 'hello';
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+  createDivsForChars(word);
 
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
